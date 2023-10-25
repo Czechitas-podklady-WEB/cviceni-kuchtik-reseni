573ebde5a2c222a9e895d75b32d923aa158873ed
Iva Havranová <havranova.i@seznam.cz>
2023-10-25 13:12:00 +0200

    Stylování formuláře.

--- src/components/FormularPridani/index.jsx
+++ src/components/FormularPridani/index.jsx
@@ -1,17 +1,33 @@
+import React from 'react';
+import './index.css';
+
 export const FormularPridani = () => {
   return (
-    <form id="formular-pridani">
-      <h2>Přidání receptu</h2>
-      <div>
-        <label htmlFor="nazev">Název</label>
-        <input type="text" id="nazev" required />
+    <form id="formular-pridani" className="formular">
+      <h2 className="formular__nadpis">Přidání receptu</h2>
+      <div className="formular__polozka">
+        <label htmlFor="nazev" className="formular__label">
+          Název
+        </label>
+        <input type="text" id="nazev" required className="formular__input" />
       </div>
-      <div>
-        <label htmlFor="porce">Počet porcí</label>
-        <input type="number" id="porce" defaultValue="4" min={1} required />
+      <div className="formular__polozka">
+        <label htmlFor="porce" className="formular__label">
+          Počet porcí
+        </label>
+        <input
+          type="number"
+          id="porce"
+          defaultValue="4"
+          min={1}
+          required
+          className="formular__input"
+        />
       </div>
-      <div>
-        <label htmlFor="doba">Doba přípravy (v minutách)</label>
+      <div className="formular__polozka">
+        <label htmlFor="doba" className="formular__label">
+          Doba přípravy (v minutách)
+        </label>
         <input
           type="number"
           id="doba"
@@ -19,26 +35,43 @@ export const FormularPridani = () => {
           min={0}
           step={10}
           required
+          className="formular__input"
         />
       </div>
-      <div>
-        <label htmlFor="ingredience">Ingredience</label>
+      <div className="formular__polozka">
+        <label htmlFor="ingredience" className="formular__label">
+          Ingredience
+        </label>
         <textarea
           id="ingredience"
           placeholder="Seznam ingrediencí, každá ingredience na jednom řádku…"
           required
+          className="formular__input formular__textarea"
         ></textarea>
       </div>
-      <div>
-        <label htmlFor="postup">Postup</label>
-        <textarea id="postup" required></textarea>
+      <div className="formular__polozka">
+        <label htmlFor="postup" className="formular__label">
+          Postup
+        </label>
+        <textarea
+          id="postup"
+          required
+          className="formular__input formular__textarea"
+        ></textarea>
       </div>
-      <div>
-        <label htmlFor="obrazek">Adresa obrázku</label>
-        <input type="obrazek" id="obrazek" required />
+      <div className="formular__polozka">
+        <label htmlFor="obrazek" className="formular__label">
+          Adresa obrázku
+        </label>
+        <input
+          type="obrazek"
+          id="obrazek"
+          required
+          className="formular__input"
+        />
       </div>
-      <div>
-        <button>Přidat</button>
+      <div className="formular__button--div">
+        <button className="formular__button">Přidat</button>
       </div>
     </form>
   );
