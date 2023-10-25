6345a09152a40b5b3ef1a0c573d7554641f8929a
Iva Havranová <havranova.i@seznam.cz>
2023-10-25 13:32:46 +0200

    Stylování odkazu.

--- src/pages/recept.jsx
+++ src/pages/recept.jsx
@@ -1,25 +1,29 @@
-import { render } from "@czechitas/render";
-import { DetailReceptu } from "../components/DetailReceptu";
-import "../global.css";
-import "./recept.css";
+import { render } from '@czechitas/render';
+import { DetailReceptu } from '../components/DetailReceptu';
+import '../global.css';
+import './recept.css';
 
 const params = new URLSearchParams(window.location.search);
-const id = params.get("id");
+const id = params.get('id');
 
 const receptResponse = await fetch(`http://localhost:4000/api/recepty/${id}`);
 const recept = (await receptResponse.json()).result;
-console.log(recept)
+console.log(recept);
 
-document.querySelector("#root").innerHTML = render(
+document.querySelector('#root').innerHTML = render(
   <>
-  <DetailReceptu
-    nazev={recept.nazev}
-    obrazek={recept.obrazek}
-    porce={recept.porce}
-    doba={recept.doba}
-    ingredience={recept.ingredience}
-    postup={recept.postup}
-  />
-  <div><a href="/">Zpět na seznam receptů</a></div>
-  </>
+    <DetailReceptu
+      nazev={recept.nazev}
+      obrazek={recept.obrazek}
+      porce={recept.porce}
+      doba={recept.doba}
+      ingredience={recept.ingredience}
+      postup={recept.postup}
+    />
+    <div className="odkaz__zpet">
+      <a href="/" className="odkaz__zpet-link">
+        Zpět na seznam receptů
+      </a>
+    </div>
+  </>,
 );
